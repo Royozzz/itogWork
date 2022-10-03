94950b23865c2cc3d8d763260993d2a623651b97
Aleksey <consultantbar@mail.ru>
2022-10-03 18:45:23 +0300

    add program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите элементы массива через пробел: ");
+
+string[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+string[] result = new string[array.Length];
+int size = 0;
+foreach (string element in array)
+{
+    if (element.Length <= 3)
+    {
+        result[size] = element;
+        size++;
+    }
+}
+
+ShowArray(array);
+Console.WriteLine();
+ShowArray(result);
